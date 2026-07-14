fa76dd9fa088a3a13bbbe7f651e338e77f7cdbb3
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-15 00:25:55 +0530

    Create Count Vowels and Consonants in a String.cpp

---
+++ Count Vowels and Consonants in a String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int vowels = 0, consonants = 0;
+
+    for (char ch : str) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || 
+            ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' ||
+            ch == 'O' || ch == 'U') {
+            vowels++;
+        }
+        else if ((ch >= 'a' && ch <= 'z') || 
+                 (ch >= 'A' && ch <= 'Z')) {
+            consonants++;
+        }
+    }
+
+    cout << "Vowels: " << vowels << endl;
+    cout << "Consonants: " << consonants;
+
+    return 0;
+}
