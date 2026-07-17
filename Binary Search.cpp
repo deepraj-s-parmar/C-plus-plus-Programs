7c1770da341f2c95decfd693915d5b1a3d84e8f4
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-18 01:12:38 +0530

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cin >> key;
+
+    int low = 0, high = n - 1;
+    bool found = false;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            cout << "Element found at index " << mid;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    if (!found)
+        cout << "Element not found";
+
+    return 0;
+}
