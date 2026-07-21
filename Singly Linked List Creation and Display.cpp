67cf286c6b008ef522b3ee272cfde38be09142cc
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-22 00:19:32 +0530

    Create Singly Linked List Creation and Display.cpp

---
+++ Singly Linked List Creation and Display.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+int main() {
+
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    head->next = second;
+    second->next = third;
+
+    Node* temp = head;
+
+    cout << "Linked List: ";
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
