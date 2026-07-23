409634fef6423dee9915786d15a3ad46ee862480
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-24 00:43:28 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -10,18 +10,18 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    int j = 0;
+    int index = 0;
 
     for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[j]) {
-            j++;
-            arr[j] = arr[i];
+        if (arr[i] != arr[index]) {
+            index++;
+            arr[index] = arr[i];
         }
     }
 
     cout << "Array after removing duplicates: ";
 
-    for (int i = 0; i <= j; i++)
+    for (int i = 0; i <= index; i++)
         cout << arr[i] << " ";
 
     return 0;
