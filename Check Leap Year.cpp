81e7a00eb12a3afccd14df5041fcafdd65d77243
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-24 00:43:21 +0530

    Update Check Leap Year.cpp

--- Check Leap Year.cpp
+++ Check Leap Year.cpp
@@ -5,7 +5,8 @@ int main() {
     int year;
     cin >> year;
 
-    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+    if ((year % 400 == 0) || 
+        (year % 4 == 0 && year % 100 != 0))
         cout << "Leap Year";
     else
         cout << "Not Leap Year";
