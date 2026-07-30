77b68bbeb02182cebffa37b1d00a35e83be5117a
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-07-31 01:04:07 +0530

    Create Kadane's Algorithm (Maximum Subarray Sum).cpp

---
+++ Kadane's Algorithm (Maximum Subarray Sum).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int maxEnding = arr[0];
+    int maxSoFar = arr[0];
+
+    for (int i = 1; i < n; i++) {
+
+        maxEnding = max(arr[i], maxEnding + arr[i]);
+
+        if (maxEnding > maxSoFar)
+            maxSoFar = maxEnding;
+    }
+
+    cout << "Maximum Sum = " << maxSoFar;
+
+    return 0;
+}
