62d408ebc27af53068e34c3e0d9be96d08c38184
Deepraj Singh Parmar <dsp154deepraj@gmail.com>
2026-08-06 12:11:49 +0530

    Create Longest Common Subsequence (LCS).cpp

---
+++ Longest Common Subsequence (LCS).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string a, b;
+
+    cin >> a >> b;
+
+    int n = a.length();
+    int m = b.length();
+
+    int dp[100][100];
+
+    for (int i = 0; i <= n; i++) {
+
+        for (int j = 0; j <= m; j++) {
+
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+
+            else if (a[i-1] == b[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << "LCS Length: " << dp[n][m];
+
+    return 0;
+}
